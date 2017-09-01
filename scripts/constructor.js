08bfb6c4f212327ac6074d5011e1ce4407b01fbd
Maryussuperboy <martsyalis@gmail.com>
2017-09-01 15:23:14 -0700

    button working

--- scripts/constructor.js
+++ scripts/constructor.js
@@ -10,19 +10,20 @@ function Project (dataArray) {
   this.title = dataArray.title;
   this.date = dataArray.date;
   this.screenShot = dataArray.screenShot;
-  this.id = dataArray.title;
+  this.id = dataArray.id;
 }
 
 Project.prototype.toScript = function() {
   var $newProject = $('.project-template').clone();
   $newProject.removeClass('project-template');
   $newProject.addClass('generated-project');
+  $newProject.attr('id', this.id);
   $newProject.find('img').attr('src',this.screenShot);
   $newProject.find('.project-title').text(this.title);
   $newProject.find('.project-date').text(this.date);
   $newProject.find('.project-description').text(this.description);
-  $newProject.find('.project-id').attr('data-id','"'+this.id+'"')
-  //console.log("new project",$newProject);
+  $newProject.find('.id').attr('href',`#${this.id+1}`)
+  console.log("new project",$newProject);
   return $newProject;
 };
 
