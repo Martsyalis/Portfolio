20484f8f1583de6b841f1ae0cb853e38219e4663
Maryussuperboy <martsyalis@gmail.com>
2017-09-01 16:53:52 -0700

    on a way to JSON

--- scripts/constructor.js
+++ scripts/constructor.js
@@ -1,42 +1,42 @@
 'use strict'
 
 
-var projectsArray = [];
+Project.all = [];
 
-
-console.log('data array',dataArray);
 
 function Project (dataArray) {
   this.title = dataArray.title;
   this.date = dataArray.date;
   this.screenShot = dataArray.screenShot;
   this.id = dataArray.id;
+  this.description = dataArray.description;
+  this.projectUrl = dataArray.projectUrl;
+  this.nextId = dataArray.id +1;
 }
 
-Project.prototype.toScript = function() {
-  var $newProject = $('.project-template').clone();
-  $newProject.removeClass('project-template');
-  $newProject.addClass('generated-project');
-  $newProject.attr('id', this.id);
-  $newProject.find('img').attr('src',this.screenShot);
-  $newProject.find('.project-title').text(this.title);
-  $newProject.find('.project-date').text(this.date);
-  $newProject.find('.project-description').text(this.description);
-  $newProject.find('.id').attr('href',`#${this.id+1}`)
-  console.log("new project",$newProject);
-  return $newProject;
-};
 
+Project.prototype.toScript = function() {
+  var template = $( '#project-template' ).html();
+  var templateFiller = Handlebars.compile( template );
+
+
+  
+
+  var filledTemplate = templateFiller( this );
+
+  $('#home-projects' ).append( filledTemplate );
+
+};
 
 dataArray.forEach(function (projectObject){
   projectsArray.push(new Project(projectObject));
 
 });
-//console.log('projects array',projectsArray);
+
 
 projectsArray.forEach(function (Project){
-  $('.home').append(Project.toScript());
-  //console.log('project',Project);
+  $('#home-projects').append(Project.toScript());
+ 
 
 });
 
