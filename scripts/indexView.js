c4f5bb30e312d3bdcd946041a7b998e04df85998
Maryussuperboy <martsyalis@gmail.com>
2017-09-11 15:04:01 -0700

    IFFE ing

--- scripts/indexView.js
+++ scripts/indexView.js
@@ -1,32 +1,30 @@
 'use strict'
 
+var app = app || {};
 
-var indexView = {};
+(function ( module ) {
 
-$(document).ready(function(){
-  
-  indexView.mainNav();
-  indexView.primer();
+  var indexView = {};
 
-});
-
-indexView.primer = function(){
-  $('.about').hide();
-  //$('.project-template').hide();
-};
-
-
-
-indexView.mainNav = function(){
-  $('.main-nav').on('click','.tab', function(){
-    $('.tab-hide').hide();
-    var clickTarget = $(this).attr( 'data-tab' );
-    // console.log(+clickTarget);
-    // console.log('section[data-tab='+clickTarget+']');
-    $('section[data-tab="'+clickTarget+'"]').fadeIn( 'medium' );
-
-
-
+  $(document).ready(function(){
+    indexView.mainNav();
+    indexView.primer();
   });
-};
 
+  indexView.primer = function(){
+    $('.about').hide();
+    //$('.project-template').hide();
+  };
+
+  indexView.mainNav = function(){
+    $('.main-nav').on('click','.tab', function(){
+      $('.tab-hide').hide();
+      var clickTarget = $(this).attr( 'data-tab' );
+      // console.log(+clickTarget);
+      // console.log('section[data-tab='+clickTarget+']');
+      $('section[data-tab="'+clickTarget+'"]').fadeIn( 'medium' );
+    });
+  };  
+
+  module.indexView = indexView;
+})(app);
